c83426e557b98e218f3f96aea8bbc5c021e72db4
Ivan Kotovich <kotovich.ivan@gmail.com>
2023-07-14 17:22:14 +0200

    commit 1st test

--- src/test/ui/first.spec.js
+++ src/test/ui/first.spec.js
@@ -1,23 +1,23 @@
 const app = require('../../app/app');
 
 describe('My First Test', () => {
-    it('Classic CY pattern', () => {
-        cy.visit('/guides/overview/why-cypress');
-        cy.get('[class*="headerWrapper"]')
-            .should('contain.text', 'Why Cypress?');
-        cy.request('GET', 'https://docs.cypress.io/')
-            .then((resp) => {
-                expect(resp.status).to.eq(200);
-            })
-        cy.wrap(app.sleep(2000))
-            .then((timeout) => {
-                expect(timeout).to.eq(2000);
-            })
-            .then(() => app.sleep(1000))
-            .then((timeout) => {
-                expect(timeout).to.eq(1000);
-            })
-    })
+    // it('Classic CY pattern', () => {
+    //     cy.visit('/guides/overview/why-cypress');
+    //     cy.get('[class*="headerWrapper"]')
+    //         .should('contain.text', 'Why Cypress?');
+    //     cy.request('GET', 'https://docs.cypress.io/')
+    //         .then((resp) => {
+    //             expect(resp.status).to.eq(200);
+    //         })
+    //     cy.wrap(app.sleep(2000))
+    //         .then((timeout) => {
+    //             expect(timeout).to.eq(2000);
+    //         })
+    //         .then(() => app.sleep(1000))
+    //         .then((timeout) => {
+    //             expect(timeout).to.eq(1000);
+    //         })
+    // })
     it('Own made Object Model', () => {
         app.ui.visit('/guides/overview/why-cypress');
         app.ui.article.get('mainTitle')
